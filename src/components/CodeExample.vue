c43c7629ce7d583a1ecab1eab2502884f4b0e1f5
zhanyi233 <2331767972@qq.com>
2022-11-04 21:20:03 +0800

    docs: 优化样式

--- src/components/CodeExample.vue
+++ src/components/CodeExample.vue
@@ -3,34 +3,40 @@
     <div class="source" v-if="component">
       <component :is="component" />
     </div>
-    <div class="op-btn">
-      <ElTooltip content="复制源代码" :show-arrow="false">
-        <el-icon
-          :size="18"
-          style="cursor: pointer; margin-right: 18px"
-          @click="copyClick"
-          ><CopyDocument
-        /></el-icon>
-      </ElTooltip>
-      <ElTooltip content="查看源代码" :show-arrow="false">
-        <el-icon
-          :size="20"
-          style="cursor: pointer"
-          @click="showCode = !showCode"
-          ><View
-        /></el-icon>
-      </ElTooltip>
-    </div>
-    <div
-      class="meta"
-      ref="meta"
-      :style="{ height: childHeight }"
-      v-if="component"
-    >
-      <div ref="codeBlock" class="demo-container-code" v-show="showCode">
-        <pre class="language-html" v-html="html" />
+    <template v-if="component">
+      <div class="op-btn">
+        <ElTooltip content="复制源代码" :show-arrow="false">
+          <el-icon
+            :size="18"
+            style="cursor: pointer; margin-right: 18px"
+            @click="copyClick"
+            ><CopyDocument
+          /></el-icon>
+        </ElTooltip>
+        <ElTooltip content="查看源代码" :show-arrow="false">
+          <el-icon
+            :size="20"
+            style="cursor: pointer"
+            @click="showCode = !showCode"
+            ><View
+          /></el-icon>
+        </ElTooltip>
       </div>
-    </div>
+      <div class="meta" ref="meta" :style="{ height: childHeight }">
+        <div ref="codeBlock" class="demo-container-code" v-show="showCode">
+          <pre class="language-html" v-html="html" />
+        </div>
+      </div>
+      <div
+        class="demo-block-control"
+        ref="control"
+        @click="showCode = !showCode"
+        v-if="showCode"
+      >
+        <el-icon><CaretTop /></el-icon>
+        <span>隐藏代码</span>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -96,7 +102,7 @@ watch(showCode, (val) => {
   }
 
   .op-btn {
-    border-top: solid 1px #eaeefb;
+    border-top: solid 1px var(--border-color);
     padding: 10px;
     display: flex;
     align-items: center;
@@ -109,6 +115,34 @@ watch(showCode, (val) => {
     overflow: hidden;
     height: 0;
     transition: height 0.2s;
+  }
+
+  .demo-block-control {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border-top: solid 1px var(--border-color);
+    height: 44px;
+    box-sizing: border-box;
+    background-color: var(--bg-color);
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    color: #d3dce6;
+    cursor: pointer;
+    position: relative;
+    font-size: 14px;
+
+    .el-icon {
+      margin-right: 5px;
+      font-size: 16px;
+      transition: 0.3s;
+    }
+
+    &:hover {
+      color: var(--color-primary);
+      background-color: #f9fafc;
+    }
   }
 }
 </style>
