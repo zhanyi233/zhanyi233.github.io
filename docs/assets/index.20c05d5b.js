3ad9c4a632dd253d048bea40f2fcb0d320575fbd
zhanyi233 <2331767972@qq.com>
2022-11-09 08:57:56 +0800

    docs: 更新默认文档

--- docs/assets/index.20c05d5b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./header.66bc9231.js";import{l as o,o as r}from"./index.88daa41c.js";const n={__name:"index",setup(_){return(a,t)=>(r(),o(e))}};export{n as default};
---
+++ docs/assets/index.6aff0f8c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./header.d97931eb.js";import{l as o,o as r}from"./index.6c208fea.js";const n={__name:"index",setup(_){return(a,t)=>(r(),o(e))}};export{n as default};
